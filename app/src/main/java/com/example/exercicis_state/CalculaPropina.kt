e556bf1c4368a3a2c6691dfb2295340ed387d117
SV-jtv <yoselyn.varela.7e6@itb.cat>
2025-03-14 13:23:08 +0100

    14/03/25

---
+++ app/src/main/java/com/example/exercicis_state/CalculaPropina.kt
@@ -0,0 +1,9 @@
+package com.example.exercicis_state
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CalculaPropina(modifier: Modifier = Modifier) {
+
+}
---
+++ app/src/main/java/com/example/exercicis_state/IMC.kt
@@ -0,0 +1,10 @@
+package com.example.exercicis_state
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun IMC(modifier: Modifier = Modifier) {
+
+}
